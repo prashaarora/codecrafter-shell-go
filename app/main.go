4fe5ba10033834854c4014fbf020927f64d1bae2
Prashant Arora <prashant.arora@trivago.com>
2025-11-09 23:35:45 +0100

    building type

--- app/main.go
+++ app/main.go
@@ -12,20 +12,46 @@ var _ = fmt.Fprint
 var _ = os.Stdout
 func main() {
 	for {
-	fmt.Fprint(os.Stdout, "$ ")
-	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading input:", err)
-		os.Exit(1)
-	}
-	if strings.TrimRight(command, "\n") == "exit 0"{
-		os.Exit(0)
-	} else if strings.HasPrefix(command, "echo"){
-       output := strings.TrimPrefix(command, "echo")
-	   fmt.Println(strings.TrimSpace(output))
-	} else {
-		fmt.Println(command[:len(command)-1] + ": command not found")
-	}		
+		fmt.Fprint(os.Stdout, "$ ")
+		//wait for user input
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
+		switch {
+			case strings.TrimRight(command, "\n") == "exit 0":
+				os.Exit(0)
+			case strings.HasPrefix(command, "echo"):
+				output := strings.TrimPrefix(command, "echo")
+				fmt.Println(strings.TrimSpace(output))
+			case strings.HasPrefix(command, "type"):
+				typeOutput := strings.TrimSpace(strings.TrimPrefix(command, "type"))
+				if typeOutput == "echo" || typeOutput == "exit" || typeOutput == "type"{
+					fmt.Println(typeOutput + " is a shell builtin")
+				} else {
+					fmt.Println(typeOutput + ": command not found")
+				}
+			default:
+				fmt.Println(command[:len(command)-1] + ": command not found")
+		}
 	}
 }
+
+	// for {
+	// fmt.Fprint(os.Stdout, "$ ")
+	// // Wait for user input
+	// command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	// if err != nil {
+	// 	fmt.Fprintln(os.Stderr, "Error reading input:", err)
+	// 	os.Exit(1)
+	// }
+	// if strings.TrimRight(command, "\n") == "exit 0"{
+	// 	os.Exit(0)
+	// } else if strings.HasPrefix(command, "echo"){
+    //    output := strings.TrimPrefix(command, "echo")
+	//    fmt.Println(strings.TrimSpace(output))
+	// } else {
+	// 	fmt.Println(command[:len(command)-1] + ": command not found")
+	// }		
+	// }
