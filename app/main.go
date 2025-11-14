aeb313d21e90e0694c2aeca5333360f69b569c7c
Prashant Arora <prashant.arora@trivago.com>
2025-11-14 17:38:07 +0100

    implementing locate executable files

--- app/main.go
+++ app/main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
-// Ensures gofmt doesn't remove the "fmt" and "os" imports in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
 var _ = os.Stdout
+func checkFileExists(filepath string)bool{
+	_, error := os.Stat(filepath)
+	return !errors.Is(error, os.ErrNotExist)
+}
 func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
@@ -27,31 +32,38 @@ func main() {
 				fmt.Println(strings.TrimSpace(output))
 			case strings.HasPrefix(command, "type"):
 				typeOutput := strings.TrimSpace(strings.TrimPrefix(command, "type"))
-				if typeOutput == "echo" || typeOutput == "exit" || typeOutput == "type"{
+				builtins := map[string]bool{
+					"echo" : true,
+					"exit" : true,
+					"type" : true,
+				}
+
+				if builtins[typeOutput]{
 					fmt.Println(typeOutput + " is a shell builtin")
 				} else {
-					fmt.Println(typeOutput + ": not found")
+					pathVar := os.Getenv("PATH")
+					pathDirs := strings.Split(pathVar, string(os.PathListSeparator))
+					found := false
+					for _, i := range pathDirs {
+						filename := filepath.Join(i, typeOutput)
+						isFileexist := checkFileExists(filename)
+						if isFileexist{
+							fileInfo, err := os.Stat(filename)
+							if err == nil {
+								if fileInfo.Mode().Perm() & 0111 != 0{
+									fmt.Println(typeOutput + " is " + filename)
+									found = true
+									break
+								}
+							}
+						} 
+					}
+					if !found{
+						fmt.Println(typeOutput + ": not found")
+					}
 				}
 			default:
 				fmt.Println(command[:len(command)-1] + ": command not found")
 		}
 	}
 }
-
-	// for {
-	// fmt.Fprint(os.Stdout, "$ ")
-	// // Wait for user input
-	// command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	// if err != nil {
-	// 	fmt.Fprintln(os.Stderr, "Error reading input:", err)
-	// 	os.Exit(1)
-	// }
-	// if strings.TrimRight(command, "\n") == "exit 0"{
-	// 	os.Exit(0)
-	// } else if strings.HasPrefix(command, "echo"){
-    //    output := strings.TrimPrefix(command, "echo")
-	//    fmt.Println(strings.TrimSpace(output))
-	// } else {
-	// 	fmt.Println(command[:len(command)-1] + ": command not found")
-	// }		
-	// }
